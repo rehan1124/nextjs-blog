f2cfbed0c6da53f10c4c42f249430929970a434b
srehan1 <syed.rehan@shoppertrak.com>
2023-05-20 18:59:35 +0530

    Link

---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const FirstPost = () => {
+  return (
+    <>
+      <h1>First post</h1>
+      <h2>
+        <Link href="/">Back to home ...</Link>
+      </h2>
+    </>
+  );
+};
+
+export default FirstPost;
