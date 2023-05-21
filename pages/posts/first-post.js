6370f8dc3fc966fe362e2cdcdf9bef30b87908a8
srehan1 <syed.rehan@shoppertrak.com>
2023-05-21 19:11:24 +0530

    Creating basic profile

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,26 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/Layout";
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
-        <title>First Post</title>
+        <title>First Post 📬</title>
       </Head>
+      <Script
+        // src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`Script loaded correctly, window.FB has been populated.`)
+        }
+      />
       <h1>First post</h1>
-      <h2>
-        <Link href="/">Back to home ...</Link>
-      </h2>
-    </>
+      {/* <h2>
+        <Link href="/">⬅ Back to home</Link>
+      </h2> */}
+    </Layout>
   );
 };
 
