727074e817ad4ced695f2ac409f21ed7be828849
srehan1 <syed.rehan@shoppertrak.com>
2023-05-22 16:40:26 +0530

    Dynamic routes done

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
+import Date from "../../components/Date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
   return (
@@ -8,13 +10,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
